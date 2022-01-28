a7fe53d98facc96b04e3418195d91623d011df5d
hitmaizer <jose.sousalves@gmail.com>
2022-01-28 21:39:54 +0000

    initial jsx markup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react"
 export default function App() {
     return (
         <div className="page--wrapper">
-            Hello mate!
+            <h1 className="intro--title">Quizzical</h1>
+            <p className="intro--description">Some description if needed</p>
+            <button className="intro--button">Start Quiz</button>
         </div>
     )
 }
