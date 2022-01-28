d93ecc8e7e4a9c814c899a89bfbe9040da8c47c3
hitmaizer <jose.sousalves@gmail.com>
2022-01-28 21:54:28 +0000

    intro page markup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react"
 
 export default function App() {
     return (
-        <div className="page--wrapper">
-            <h1 className="intro--title">Quizzical</h1>
-            <p className="intro--description">Some description if needed</p>
-            <button className="intro--button">Start Quiz</button>
+        <div className="quiz--wrapper">
+            <div className="intro--wrapper">
+                <h1 className="intro--title">Quizzical</h1>
+                <p className="intro--description">Some description if needed</p>
+                <button className="intro--button">Start Quiz</button>
+            </div>
         </div>
     )
 }
