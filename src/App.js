ac34ef4e35e7818e6a83069a831082bcc4fe8e9a
hitmaizer <jose.sousalves@gmail.com>
2022-01-29 16:20:47 +0000

    getRandomQuestions from API function

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import Intro from "./components/Intro"
 import Card from "./components/Card"
 
 export default function App() {
-    const [quizData, setQuizData] = React.useState({})
+    const [quizData, setQuizData] = React.useState([])
+    const [questions, setQuestions] = React.useState([])
+
     const [intro, setIntro] = React.useState(true)
+    
 
     React.useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=40")
             .then(res => res.json())
             .then(data => setQuizData(data.results))
-    }, [])
 
+        getRandomQuestions()
+            
+
+    }, [intro])
     
+    function getRandomQuestions() {
+        const newQuestions = []
+        for (let i = 0; i < 4; i++) {
+            const randomNumber = Math.floor(Math.random() * quizData.length)
+            const randomQuestion = quizData[randomNumber]
+            newQuestions.push(randomQuestion)
+        }
+        setQuestions(newQuestions)
+    }
+
+    console.log(questions)
+
 
     function toggleIntro() {
         setIntro(false)
         
     }
 
-    console.log(quizData)
+    
 
     return (
         <div className="quiz--wrapper">
