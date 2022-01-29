52ffd7ae325c8b1aafc1e710f95148d62e6ff296
hitmaizer <jose.sousalves@gmail.com>
2022-01-29 18:28:43 +0000

    getRandomQuestions fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import Card from "./components/Card"
 
 export default function App() {
     const [quizData, setQuizData] = React.useState([])
-    const [questions, setQuestions] = React.useState([])
+    const [questions, setQuestions] = React.useState([
+        {
+            category: "",
+            type: "",
+            difficulty: "",
+            question: "",
+            correct_answer: "",
+            incorrect_answers: [
+            "",
+            "",
+            ""
+            ]
+        }
+    ])
 
     const [intro, setIntro] = React.useState(true)
+    
     
 
     React.useEffect(() => {
@@ -14,28 +28,39 @@ export default function App() {
             .then(res => res.json())
             .then(data => setQuizData(data.results))
 
-        getRandomQuestions()
+             
             
 
-    }, [intro])
-    
+    }, [])
+
     function getRandomQuestions() {
         const newQuestions = []
-        for (let i = 0; i < 4; i++) {
-            const randomNumber = Math.floor(Math.random() * quizData.length)
-            const randomQuestion = quizData[randomNumber]
-            newQuestions.push(randomQuestion)
-        }
+            for (let i = 0; i < 4; i++) {
+                const randomNumber = Math.floor(Math.random() * quizData.length)
+                const randomQuestion = quizData[randomNumber]
+                newQuestions.push(randomQuestion)
+            }
         setQuestions(newQuestions)
+        
     }
+
+    
+
+    function toggleIntro() {
+        setIntro(!intro)
+        getRandomQuestions()
+    }
+    
+
 
     console.log(questions)
 
-
-    function toggleIntro() {
-        setIntro(false)
-        
-    }
+    const cardElements = questions.map(question => 
+    <Card 
+       question={question.question} 
+       
+    />)
+    
 
     
 
@@ -47,15 +72,15 @@ export default function App() {
                 clickHandler={toggleIntro}
             />}
 
+            
+            
             {!intro && 
-            <div className="quiz--wrapper">
-                <Card />
-                <Card />,
-                <Card />,
-                <Card />,
-                <button className="quiz--checkbutton">Check answers</button>
-            </div>
+                <div className="quiz--wrapper">
+                    {cardElements}
+                    <button className="quiz--checkbutton">Check answers</button>
+                </div>
             }
+            
 
         </div>
     )
