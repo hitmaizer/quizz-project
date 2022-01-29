d8a6f609a3b1162007a76dbb762bb908929e753b
hitmaizer <jose.sousalves@gmail.com>
2022-01-29 12:48:06 +0000

    moved to separate component

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Intro() {
+    return (
+    <div className="intro--wrapper">
+        <h1 className="intro--title">Quizzical</h1>
+        <p className="intro--description">Some description if needed</p>
+        <button className="intro--button">Start Quiz</button>
+    </div>
+    )
+}
