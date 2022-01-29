93fdb3a90d7f25a9cad35d65c3567213e8f135f1
hitmaizer <jose.sousalves@gmail.com>
2022-01-29 14:34:00 +0000

    fetch data api

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,15 @@
 import React from "react"
 
-export default function Intro() {
+export default function Intro(props) {
     return (
     <div className="intro--wrapper">
         <h1 className="intro--title">Quizzical</h1>
         <p className="intro--description">Some description if needed</p>
-        <button className="intro--button">Start Quiz</button>
+        <button 
+            onClick={props.clickHandler} 
+            className="intro--button">
+            Start Quiz
+        </button>
     </div>
     )
 }
