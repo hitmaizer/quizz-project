3a3755355a51591e58006704cb7bd8bc3e0f5cc9
hitmaizer <jose.sousalves@gmail.com>
2022-01-30 14:59:48 +0000

    fix code to move the function up

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,21 @@ import React from "react"
 
 export default function Card(props) {
 
+    const shuffledAnswers = [props.correct, ...props.wrong].sort(() => Math.random() - 0.5)
+
     return (
         
         <div className="card--wrapper">
             <h1 className="card--question" dangerouslySetInnerHTML={{ __html: props.question}}/>
             <div className="card--answers">
-                <button className="card--button">{props.correct}</button>
-                <button className="card--button">{props.wrong[2]}</button>
-                <button className="card--button">{props.wrong[0]}</button>
-                <button className="card--button">{props.wrong[1]}</button>
+                <button className="card--button" onClick={() => props.clickHandler(shuffledAnswers[0])} 
+                dangerouslySetInnerHTML={{ __html: shuffledAnswers[0]}}/>
+                <button className="card--button" onClick={() => props.clickHandler(shuffledAnswers[1])}
+                dangerouslySetInnerHTML={{ __html: shuffledAnswers[1]}}/>
+                <button className="card--button" onClick={() => props.clickHandler(shuffledAnswers[2])}
+                dangerouslySetInnerHTML={{ __html: shuffledAnswers[2]}}/>
+                <button className="card--button" onClick={() => props.clickHandler(shuffledAnswers[3])}
+                dangerouslySetInnerHTML={{ __html: shuffledAnswers[3]}}/>
             </div>
             <hr className="card--line"/>
         </div>
