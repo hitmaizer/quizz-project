9e4090a38b76c7f90e07434bd5d9ee5c19e1a42a
hitmaizer <jose.sousalves@gmail.com>
2022-01-30 22:44:18 +0000

    2 different approaches

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,39 +1,58 @@
 import React from "react"
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid"
+
 
 export default function Card(props) {
     let shuffledAnswers = [props.correct, ...props.wrong]
-    
-    
-    
-    React.useState(() => {
+    const [isActive, setActive] = React.useState(false)
+    React.useEffect(() => {
         let shuffledAnswers = [props.correct, ...props.wrong].sort(() => Math.random() - 0.5)
         
     },[])
 
-    
-    const styles = {
-        backgroundColor: props.selected ? "#D6DBF5" : "#fff"
+    const toggleClass = () => {
+        setActive(!isActive)
     }
 
-    console.log(props.id)
+    /* function setAnswers() {
+        const newArr = []
+        newArr.push(shuffledAnswers.map(item => {
+            return {...item, id: nanoid()}
+        }))
+        setAnswerElements(newArr)
+    } */
+    
+    /* const answerElements = shuffledAnswers.map(item => 
+    <button className="card--button" 
+        key={nanoid()}
+        id={nanoid()}
+        dangerouslySetInnerHTML={{ __html: item}}
+        
+    />) */
 
+    // console.log(answerElements)
+
+    
+    const styles = { backgroundColor: props.selected ? "#D6DBF5" : "#fff"}
+    
     return (
         
         <div className="card--wrapper">
             <h1 className="card--question" dangerouslySetInnerHTML={{ __html: props.question}}/>
             <div className="card--answers">
-                <button className="card--button" style={styles} onClick={() => props.clickHandler(props.id, shuffledAnswers[0])} 
+                
+                {/* {answerElements} */}
+                <button className={isActive ? "active card--button" : "card--button"} onClick={toggleClass} 
                 dangerouslySetInnerHTML={{ __html: shuffledAnswers[0]}}/>
                 <button className="card--button" style={styles} onClick={() => props.clickHandler(props.id, shuffledAnswers[1])} 
                 dangerouslySetInnerHTML={{ __html: shuffledAnswers[1]}}/>
                 <button className="card--button" style={styles} onClick={() => props.clickHandler(props.id, shuffledAnswers[2])} 
                 dangerouslySetInnerHTML={{ __html: shuffledAnswers[2]}}/>
                 <button className="card--button" style={styles} onClick={() => props.clickHandler(props.id, shuffledAnswers[3])} 
-                dangerouslySetInnerHTML={{ __html: shuffledAnswers[3]}}/>
+                dangerouslySetInnerHTML={{ __html: shuffledAnswers[3]}}/> 
                 
             </div>
-            <hr className="card--line"/>
+            <hr className="card--line" onClick={() => props.clickHandler(props.id, shuffledAnswers)}/>
         </div>
     )
 }
