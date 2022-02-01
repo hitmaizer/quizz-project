7a7e527eeb454d5dc28b74cf9b7ef60d0950479e
hitmaizer <jose.sousalves@gmail.com>
2022-02-01 16:53:20 +0000

    text change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ export default function Intro(props) {
     return (
     <div className="intro--wrapper">
         <h1 className="intro--title">Quizzical</h1>
-        <p className="intro--description">Some description if needed</p>
+        <p className="intro--description">4 random questions to test your cultural level!</p>
         <button 
             onClick={props.clickHandler} 
             className="intro--button">
